21a2756b70f7e186d0f4d66aefd535965f61f36d
nitins1 <nitin.sampathi@gmail.com>
2018-01-13 22:37:25 -0500

    took xhr req out of componentDidUpdate function to prevent infinite loop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import xhr from 'xhr';
 import './App.css';
-var htmlToJson = require("html-to-json")
 
 export class WeatherApp extends Component {
   constructor(props) {
@@ -14,54 +13,67 @@ export class WeatherApp extends Component {
     };
   }
 
-componentDidMount() {
-  if (window.navigator.geolocation) { // if geolocation is supported
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null
-        });
-      },
-      (error) => {
-        this.setState({
-          error: error.message,
-        });
-      }
-    );
-  } else {
-    // IP Fallback
+  populateData() {
+    //debugger;
+    let urlPrefix = "http://api.openweathermap.org/data/2.5/weather?";
+
+    let lat = 'lat=' + this.state.latitude;
+    let lon = 'lon=' + this.state.longitude;
+
+    let apiKey = 'aca10c3987b461277deb339c916a5c20'
+
+    let otherApiKey = '70f1a80f7be9d0f99a01693ffe6fedf1'
+
+    let urlSuffix = '&APPID=' + otherApiKey;
+
+    //let url = 'http://api.openweathermap.org/data/2.5/weather?lat=39.3096368&lon=-76.6298302&APPID=70f1a80f7be9d0f99a01693ffe6fedf1'
+    let url = urlPrefix + lat + '&' + lon + urlSuffix;
+    let self = this;
+    console.log(url);
+
+    xhr({
+      url: url
+    }, function (err, data) {
+      // console.log(data);
+      // console.log(typeof data.body);
+      // console.log(data.body);
+      let jsonData = JSON.parse(data.body);
+      console.log(jsonData.name);
+      console.log(self.state);
+      self.setState({
+        data: JSON.parse(data.body)
+      });
+      console.log(self.state);
+    });
   }
-}
 
-//TODO: Fetch coordinates after getCurrentPosition
-//TODO: Figure out why xhr request is returning HTML
+  componentDidMount() {
+    if (window.navigator.geolocation) { // if geolocation is supported
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            error: null
+          });
+          this.populateData();
+        },
+        (error) => {
+          this.setState({
+            error: error.message,
+          });
+        }
+      );
+    } else {
+      // IP Fallback
+    }
+  }
 
-componentDidUpdate() {
-  //debugger;
-  let urlPrefix = "http://api.openweathermap.org/data/2.5/weather?";
-  let urlSuffix = '&APPID=70f1a80f7be9d0f99a01693ffe6fedf1'
-  let lat = 'lat=' + this.state.latitude;
-  let lon = 'lon=' + this.state.longitude;
-  //let url = 'http://api.openweathermap.org/data/2.5/weather?lat=39.3096368&lon=-76.6298302&APPID=70f1a80f7be9d0f99a01693ffe6fedf1'
-  let url = urlPrefix + lat + '&' + lon + urlSuffix;
-  let self = this;
-  console.log(url);
-  xhr({
-    url: url
-  }, function (err, data) {
-    // console.log(data);
-    // console.log(typeof data.body);
-    // console.log(data.body);
-    let jsonData = JSON.parse(data.body);
-    // console.log(jsonData.name);
-    console.log(this.state)
-    /* self.setState({
-      data: JSON.parse(data.body)
-    }); */
-  });
-};
+  //TODO: Fetch coordinates after getCurrentPosition
+  //TODO: Figure out why xhr request is returning HTML
+
+  componentDidUpdate() {
+  };
 
   render() {
     var currentTemp = 'Temp will go here after fetching coordinates';
@@ -72,7 +84,7 @@ componentDidUpdate() {
         <div>
           <h1>{this.state.latitude}</h1>
           <h1>{this.state.longitude}</h1>
-          <h1>{this.state.currentCity}</h1>
+          <h1>{this.state.data.name}</h1>
           <h1>{this.state.currentWeather}</h1>
           <h1>{ currentTemp }{this.state.tempFahrenheit}</h1>
         </div>
